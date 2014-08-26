6bf49153bc42e87f9c27978d16194bd58d8d19f6
Dustin Lewis <d.lew23@gmail.com>
2014-08-25 20:53:26 -0700

    initialize roster class associations.

--- lib/roster.rb
+++ lib/roster.rb
@@ -1,4 +1,5 @@
 class Roster < ActiveRecord::Base
-
-
+  belongs_to :team
+  belongs_to :player
+  belongs_to :position
 end
--- spec/roster_spec.rb
+++ spec/roster_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Roster do
-
-
+  it { should belong_to :team }
+  it { should belong_to :player }
+  it { should belong_to :position }
 end
