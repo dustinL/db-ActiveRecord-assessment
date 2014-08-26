f82cec4b7e15d02bfc0e5b86cf10f318961613dc
Dustin Lewis <d.lew23@gmail.com>
2014-08-25 20:29:56 -0700

    initialize position class associations.

--- lib/position.rb
+++ lib/position.rb
@@ -1,4 +1,4 @@
 class Position < ActiveRecord::Base
-
-
+  has_many :players
+  has_many :rosters
 end
--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
 describe Position do
-
-
+  it { should have_many :players}
+  it { should have_many :rosters}
 end
